b221638d5f0f9284a0f1800ebf8219e24953f731
frymmox <frymmox@gmail.com>
2020-10-15 22:21:27 +0300

    fix: current edits

--- src/js/tippy.js
+++ src/js/tippy.js
@@ -1,3 +1,17 @@
-tippy('[data-tippy-content]',{
-  placement: 'bottom-start',
-})
+(function() {
+  tippy('[data-tippy-content]',{
+    placement: 'bottom-start',
+  })
+
+  const instance = tippy(document.querySelector('.show-tooltip'), {
+    placement: 'bottom-start',
+  })
+
+  instance.show()
+
+  document.querySelectorAll('[data-tippy-content]').forEach((el) => {
+    el.addEventListener("mouseenter", () => {
+      instance.hide()
+    })
+  })
+})();
