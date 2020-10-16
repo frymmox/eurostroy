358ea92b839273cee59717aec9e70200c8b0cba3
frymmox <frymmox@gmail.com>
2020-10-16 17:21:23 +0300

    fix: current edits

---
+++ src/js/swiper.js
@@ -0,0 +1,87 @@
+const swiperPromo = new Swiper('.promo__slider', {
+  pagination: {
+    el: '.promo__pagination',
+  },
+
+  navigation: {
+    nextEl: '.promo__next',
+    prevEl: '.promo__prev',
+  },
+})
+
+const swiperProjectCard = new Swiper('.project-card__slider', {
+  pagination: {
+    el: '.project-card__pagination',
+    clickable: true
+  },
+})
+
+const swiperImplementedProject = new Swiper('.implemented-project__slider', {
+  slidesPerView: 1,
+  spaceBetween: 40,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    }
+  },
+
+  navigation: {
+    nextEl: '.implemented-project__next',
+    prevEl: '.implemented-project__prev',
+  },
+})
+
+const swiperVideo = new Swiper('.video__slider', {
+  slidesPerView: 1,
+  spaceBetween: 32,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    }
+  },
+
+  navigation: {
+    nextEl: '.video__next',
+    prevEl: '.video__prev',
+  },
+})
+
+const swiperNews = new Swiper('.news__slider', {
+  slidesPerView: 1,
+  spaceBetween: 32,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    }
+  },
+
+  navigation: {
+    nextEl: '.news__next',
+    prevEl: '.news__prev',
+  },
+})
+
+const swiperConstructionProgress = new Swiper('.construction-progress__slider', {
+  slidesPerView: 1,
+  spaceBetween: 32,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    }
+  },
+
+  navigation: {
+    nextEl: '.construction-progress__next',
+    prevEl: '.construction-progress__prev',
+  },
+})
--- src/js/tippy.js
+++ src/js/tippy.js
@@ -1,17 +1,21 @@
 (function() {
-  tippy('[data-tippy-content]',{
-    placement: 'bottom-start',
-  })
-
-  const instance = tippy(document.querySelector('.show-tooltip'), {
-    placement: 'bottom-start',
-  })
-
-  instance.show()
-
-  document.querySelectorAll('[data-tippy-content]').forEach((el) => {
-    el.addEventListener("mouseenter", () => {
-      instance.hide()
+  if (document.querySelectorAll('[data-tippy-content]')) {
+    tippy('[data-tippy-content]',{
+      placement: 'bottom-start',
     })
-  })
+
+    if (document.querySelector('.show-tooltip')) {
+      const instance = tippy(document.querySelector('.show-tooltip'), {
+        placement: 'bottom-start',
+      })
+
+      instance.show();
+
+      document.querySelectorAll('[data-tippy-content]').forEach((el) => {
+        el.addEventListener("mouseenter", () => {
+          instance.hide()
+        })
+      })
+    }
+  }
 })();
