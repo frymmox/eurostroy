7e4e2cfceb2c0ac106e1490e1ae5e6299d53ae3b
frymmox <frymmox@gmail.com>
2020-10-21 12:05:50 +0300

    fix: current edits

---
+++ src/js/gallery.js
@@ -0,0 +1,25 @@
+(function() {
+  if (document.querySelector('.complex-about-media__card--gallery')) {
+    const targetGallery = document.querySelector('.complex-about-media__card--gallery')
+    targetGallery.addEventListener('click', () => {
+      $.fancybox.open([
+        {
+          src  : 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
+          opts : {
+            caption : 'First caption',
+            thumb   : 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+          }
+        },
+        {
+          src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+          opts : {
+            caption : 'Second caption',
+            thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+          }
+        }
+      ], {
+        loop : true,
+      });
+    })
+  }
+})();
