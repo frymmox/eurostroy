febc65933ea98e179ec3dc7eb562ee0fd2f5a81b
frymmox <frymmox@gmail.com>
2020-10-22 11:05:26 +0300

    fix: modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,27 +1,29 @@
 // Modal section
-const modalOpenSection = document.querySelector('.js-modal-section-open')
-const modalCloseSection = document.querySelector('.js-modal-section-close')
-const overlaySection = document.querySelector('.js-modal-section .modal__overlay')
-const modalSection = document.querySelector('.js-modal-section')
+if (document.querySelector('.modal')) {
+  const modalOpenSection = document.querySelector('.js-modal-section-open')
+  const modalCloseSection = document.querySelector('.js-modal-section-close')
+  const overlaySection = document.querySelector('.js-modal-section .modal__overlay')
+  const modalSection = document.querySelector('.js-modal-section')
 
-modalOpenSection.addEventListener('click', () => {
-  modalSection.classList.add('modal--show')
-  scrollLock.disablePageScroll()
-})
+  modalOpenSection.addEventListener('click', () => {
+    modalSection.classList.add('modal--show')
+    scrollLock.disablePageScroll()
+  })
 
-modalCloseSection.addEventListener('click', () => {
-  modalSection.classList.remove('modal--show')
-  scrollLock.enablePageScroll()
-})
-
-overlaySection.addEventListener('click', () => {
-  modalSection.classList.remove('modal--show')
-  scrollLock.enablePageScroll()
-})
-
-document.addEventListener('keydown', (e) => {
-  if (e.keyCode === 27) {
+  modalCloseSection.addEventListener('click', () => {
     modalSection.classList.remove('modal--show')
     scrollLock.enablePageScroll()
-  }
-})
+  })
+
+  overlaySection.addEventListener('click', () => {
+    modalSection.classList.remove('modal--show')
+    scrollLock.enablePageScroll()
+  })
+
+  document.addEventListener('keydown', (e) => {
+    if (e.keyCode === 27) {
+      modalSection.classList.remove('modal--show')
+      scrollLock.enablePageScroll()
+    }
+  })
+}
