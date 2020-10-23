2e05ebcec1d7a04474e613721d7d9d1ce7d34242
frymmox <frymmox@gmail.com>
2020-10-23 17:59:28 +0300

    fix: current edits

--- src/js/tabs-apartment.js
+++ src/js/tabs-apartment.js
@@ -1,16 +1,18 @@
-const viewApartment = document.querySelector('.js-view-apartment')
-const viewFloor = document.querySelector('.js-view-floor')
+if (document.querySelector('.chosen-apartment')) {
+  const viewApartment = document.querySelector('.js-view-apartment')
+  const viewFloor = document.querySelector('.js-view-floor')
 
-viewApartment.addEventListener('click', () => {
-  if (viewApartment.firstElementChild.checked) {
-    document.querySelector('.chosen-apartment__image').lastElementChild.style.display='none'
-    document.querySelector('.chosen-apartment__image').firstElementChild.style.display='block'
-  }
-})
+  viewApartment.addEventListener('click', () => {
+    if (viewApartment.firstElementChild.checked) {
+      document.querySelector('.chosen-apartment__image').lastElementChild.style.display='none'
+      document.querySelector('.chosen-apartment__image').firstElementChild.style.display='block'
+    }
+  })
 
-viewFloor.addEventListener('click', () => {
-  if (viewFloor.firstElementChild.checked) {
-    document.querySelector('.chosen-apartment__image').firstElementChild.style.display='none'
-    document.querySelector('.chosen-apartment__image').lastElementChild.style.display='block'
-  }
-})
+  viewFloor.addEventListener('click', () => {
+    if (viewFloor.firstElementChild.checked) {
+      document.querySelector('.chosen-apartment__image').firstElementChild.style.display='none'
+      document.querySelector('.chosen-apartment__image').lastElementChild.style.display='block'
+    }
+  })
+}
